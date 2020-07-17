f3e40c0a02daa1c26b9412dc473405743e14c062
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-07-17 18:14:52 +0530

    code added

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// jquery - js lib
+// dom munipulation
+//event handing
+//animation
+// easy ajax call
+//$-access or define jquery
+        // selector 
+        // . dot
+        // method
+        // optional parameter  eg object,method,arrar
+        // ;
+        // Doctor Smith Doen't make pretty snowman
+        // $(document).ready(function()
+        // {
+
+
+        //   $(p).css('color','red');
+        // });
+        // document is root element = ready
+        //html=== inner html
+        // show hide toggle method
+        //val used  to get element value
